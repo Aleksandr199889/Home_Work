a857955579031827fc306ac81033efcfd6c9df8d
Aleksandr <aleks.remiks@mail.ru>
2022-12-01 18:47:11 +0300

    Task1

---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine(number1);
+}
+else
+{
+    if (number1 < number2)
+    {
+        Console.WriteLine(number2);
+    }
+    else
+    {
+        Console.WriteLine("Числа равны");
+    }
+}
